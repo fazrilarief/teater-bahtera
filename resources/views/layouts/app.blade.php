fb0eccae4de4ac57d244cec642f8f4943c78299e
Fazril Arief Nugraha <fazrilarief331@gmail.com>
2024-01-09 12:55:42 +0700

    setup template project by Adminkit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('includes.head')
+</head>
+
+<body>
+    <div class="wrapper">
+        @include('includes.sidebar')
+        <div class="main">
+            @include('includes.navbar')
+            @yield('content')
+            @include('includes.footer')
+        </div>
+    </div>
+    @include('includes.script')
+</body>
+
+</html>
