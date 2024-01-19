3015a94cd0c11a40dae783cb5270b0970ddc5d22
Fazril Arief Nugraha <fazrilarief331@gmail.com>
2024-01-19 08:58:22 +0700

    feat(member-form): add old() method in form create member

--- resources/views/pages/admin/data-anggota/form.blade.php
+++ resources/views/pages/admin/data-anggota/form.blade.php
@@ -42,7 +42,8 @@
                             <div class="col-6">
                                 <div class="mb-3">
                                     <label for="name" class="form-label">Nama Lengkap</label>
-                                    <input type="text" name="name" class="form-control" autofocus>
+                                    <input type="text" name="name" class="form-control" autofocus
+                                        value="{{ old('name') }}">
                                 </div>
                                 @error('name')
                                     <div class="alert alert-danger p-1" role="alert"><i class="align-middle"
@@ -53,8 +54,9 @@
                                     <label for="gender" class="form-label">Jenis Kelamin</label>
                                     <select name="gender" id="gender" class="form-select">
                                         <option selected disabled>Pilih Jenis Kelamin</option>
-                                        <option value="Pria">Pria</option>
-                                        <option value="Wanita">Wanita</option>
+                                        <option value="Pria" {{ old('gender') == 'Pria' ? 'selected' : '' }}>Pria</option>
+                                        <option value="Wanita" {{ old('gender') == 'Wanita' ? 'selected' : '' }}>Wanita
+                                        </option>
                                     </select>
                                 </div>
                                 @error('gender')
@@ -65,17 +67,17 @@
                                 <div class="mb-3">
                                     <label for="nis" class="form-label">NIS</label>
                                     <input type="number" id="nis" name="nis" class="form-control"
-                                        placeholder="3412*******" minlength="0" maxlength="13">
+                                        placeholder="3412*******" minlength="0" maxlength="13" value="{{ old('nis') }}">
                                 </div>
                                 <div class="mb-3">
                                     <label for="nisn" class="form-label">NISN</label>
                                     <input type="number" id="nisn" name="nisn" class="form-control"
-                                        placeholder="006*******" minlength="0" maxlength="10">
+                                        placeholder="006*******" minlength="0" maxlength="10" value="{{ old('nisn') }}">
                                 </div>
                                 <div class="mb-3">
                                     <label for="whatsapp" class="form-label">No Whatsapp</label>
                                     <input type="number" id="whatsapp" name="whatsapp" class="form-control"
-                                        placeholder="08821313148">
+                                        placeholder="08821313148" value="{{ old('whatsapp') }}">
                                 </div>
                                 @error('whatsapp')
                                     <div class="alert alert-danger p-1" role="alert"><i class="align-middle"
@@ -84,14 +86,15 @@
                                 @enderror
                                 <div class="mb-3">
                                     <label for="email" class="form-label">Email</label>
-                                    <input type="email" name="email" class="form-control"
-                                        placeholder="ucok21@gmail.com">
+                                    <input type="email" name="email" class="form-control" placeholder="ucok21@gmail.com"
+                                        value="{{ old('email') }}">
                                 </div>
                             </div>
                             <div class="col-6">
                                 <div class="mb-3">
                                     <label for="birthday" class="form-label">Tanggal Lahir</label>
-                                    <input type="date" name="birthday" class="form-control">
+                                    <input type="date" name="birthday" class="form-control"
+                                        value="{{ old('birthday') }}">
                                 </div>
                                 @error('birthday')
                                     <div class="alert alert-danger p-1" role="alert"><i class="align-middle"
@@ -102,9 +105,9 @@
                                     <label for="grade" class="form-label">Kelas</label>
                                     <select name="grade" id="grade" class="form-select">
                                         <option selected disabled>Pilih Kelas</option>
-                                        <option value="10">10</option>
-                                        <option value="11">11</option>
-                                        <option value="12">12</option>
+                                        <option value="10" {{ old('grade') == '10' ? 'selected' : '' }}>10</option>
+                                        <option value="11" {{ old('grade') == '11' ? 'selected' : '' }}>11</option>
+                                        <option value="12" {{ old('grade') == '12' ? 'selected' : '' }}>12</option>
                                     </select>
                                 </div>
                                 @error('grade')
@@ -116,10 +119,16 @@
                                     <label for="major" class="form-label">Jurusan</label>
                                     <select name="major" id="major" class="form-select">
                                         <option selected disabled>Pilih Jurusan</option>
-                                        <option value="MIA/IPA">MIA / IPA</option>
-                                        <option value="IIS/IPS">IIS / IPS</option>
-                                        <option value="Bahasa">Bahasa</option>
-                                        <option value="Belum Memiliki Jurusan">Belum Memiliki Jurusan</option>
+                                        <option value="MIA/IPA" {{ old('major') == 'MIA/IPA' ? 'selected' : '' }}>MIA /
+                                            IPA</option>
+                                        <option value="IIS/IPS" {{ old('major') == 'IIS/IPS' ? 'selected' : '' }}>IIS /
+                                            IPS</option>
+                                        <option value="Bahasa" {{ old('major') == 'Bahasa' ? 'selected' : '' }}>Bahasa
+                                        </option>
+                                        <option value="Belum Memiliki Jurusan"
+                                            {{ old('major') == 'Belum Memiliki Jurusan' ? 'selected' : '' }}>Belum Memiliki
+                                            Jurusan
+                                        </option>
                                     </select>
                                 </div>
                                 @error('major')
@@ -132,7 +141,8 @@
                                     <select name="class_code" id="class-code" class="form-select">
                                         <option selected disabled>Pilih Kode Kelas</option>
                                         @for ($i = 1; $i <= 10; $i++)
-                                            <option value="{{ $i }}">
+                                            <option value="{{ $i }}"
+                                                {{ old('class_code') == $i ? 'selected' : '' }}>
                                                 {{ $i }}
                                             </option>
                                         @endfor
@@ -147,8 +157,12 @@
                                     <label for="structure" class="form-label">Kepanitiaan</label>
                                     <select name="structure" id="grade" class="form-select">
                                         <option selected disabled>Jabatan</option>
-                                        <option value="Anggota">Anggota</option>
-                                        <option value="Pengurus">Pengurus</option>
+                                        <option value="Anggota" {{ old('structure') == 'Anggota' ? 'selected' : '' }}>
+                                            Anggota
+                                        </option>
+                                        <option value="Pengurus" {{ old('structure') == 'Pengurus' ? 'selected' : '' }}>
+                                            Pengurus
+                                        </option>
                                     </select>
                                 </div>
                                 @error('structure')
@@ -160,10 +174,14 @@
                                     <label for="interest" class="form-label">Minat Seni</label>
                                     <select name="interest" id="grade" class="form-select">
                                         <option selected disabled>Pilih Minat Seni</option>
-                                        <option value="Puisi">Puisi</option>
-                                        <option value="Menulis">Menulis</option>
-                                        <option value="Akting">Akting</option>
-                                        <option value="Monolog">Monolog</option>
+                                        <option value="Puisi" {{ old('interest') == 'Puisi' ? 'selected' : '' }}>Puisi
+                                        </option>
+                                        <option value="Menulis" {{ old('interest') == 'Menulis' ? 'selected' : '' }}>
+                                            Menulis</option>
+                                        <option value="Akting" {{ old('interest') == 'Akting' ? 'selected' : '' }}>Akting
+                                        </option>
+                                        <option value="Monolog" {{ old('interest') == 'Monolog' ? 'selected' : '' }}>
+                                            Monolog</option>
                                     </select>
                                 </div>
                                 @error('interest')
