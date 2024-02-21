75e196b2261460a138e8d85a921b5c7f15551317
Fazril Arief Nugraha <fazrilarief331@gmail.com>
2024-02-21 07:25:41 +0700

    style (navbar) : change user info responsive for mobile

--- database/seeders/DummyUserSeeder.php
+++ database/seeders/DummyUserSeeder.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DefaultUsersSeeder extends Seeder
+class DummyUserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
