7a14bdab7df0872fa42a905dbb1f62bf406c489d
Fazril Arief Nugraha <fazrilarief331@gmail.com>
2024-07-28 22:39:10 +0700

    refactor: change dashboard view

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -8,8 +8,9 @@
                 <div class="col-xl-12 col-xxl-12 d-flex">
                     <div class="w-100">
                         <div class="row">
-                            <div class="col-sm-6">
-                                <div class="card">
+                            <div class="col-12 d-flex justify-content-between gap-2">
+                                <div
+                                    class="card col-4 border border-4 border-top-0 border-start-0 border-end-0 border-primary">
                                     <div class="card-body">
                                         <div class="row">
                                             <div class="col mt-0">
@@ -23,143 +24,48 @@
                                             </div>
                                         </div>
                                         <h1 class="mt-1 mb-3">{{ $totalMember }}</h1>
-                                        {{-- <div class="mb-0">
-                                            <span class="text-danger">
-                                                <i class="mdi mdi-arrow-bottom-right"></i> -3.65%
-                                            </span>
-                                            <span class="text-muted">Since last week</span>
-                                        </div> --}}
                                     </div>
                                 </div>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col mt-0">
-                                                <h5 class="card-title">Kriteria</h5>
-                                            </div>
-                                            <div class="col-auto">
-                                                <div class="stat text-primary">
-                                                    <i class="align-middle" data-feather="square"></i>
+                                <div
+                                    class="card col-4 border border-4 border-top-0 border-start-0 border-end-0 border-success">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col mt-0">
+                                                    <h5 class="card-title">Kriteria</h5>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <div class="stat text-primary">
+                                                        <i class="align-middle" data-feather="square"></i>
+                                                    </div>
                                                 </div>
                                             </div>
+                                            <h1 class="mt-1 mb-3">{{ $totalCriteria }}</h1>
                                         </div>
-                                        <h1 class="mt-1 mb-3">{{ $totalCriteria }}</h1>
-                                        {{-- <div class="mb-0">
-                                            <span class="text-success">
-                                                <i class="mdi mdi-arrow-bottom-right"></i> 5.25%
-                                            </span>
-                                            <span class="text-muted">Since last week</span>
-                                        </div> --}}
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col mt-0">
-                                                <h5 class="card-title">Periode</h5>
-                                            </div>
-                                            <div class="col-auto">
-                                                <div class="stat text-primary">
-                                                    <i class="align-middle" data-feather="calendar"></i>
+                                <div
+                                    class="card col-4 border border-4 border-top-0 border-start-0 border-end-0 border-warning">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col mt-0">
+                                                    <h5 class="card-title">Periode</h5>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <div class="stat text-primary">
+                                                        <i class="align-middle" data-feather="calendar"></i>
+                                                    </div>
                                                 </div>
                                             </div>
+                                            <h1 class="mt-1 mb-3">{{ $totalPeriod }}</h1>
                                         </div>
-                                        <h1 class="mt-1 mb-3">{{ $totalPeriod }}</h1>
-                                        {{-- <div class="mb-0">
-                                            <span class="text-success">
-                                                <i class="mdi mdi-arrow-bottom-right"></i> 6.65%
-                                            </span>
-                                            <span class="text-muted">Since last week</span>
-                                        </div> --}}
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col mt-0">
-                                                <h5 class="card-title">Selesai Hitung</h5>
-                                            </div>
-
-                                            <div class="col-auto">
-                                                <div class="stat text-primary">
-                                                    <i class="align-middle" data-feather="check"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <h1 class="mt-1 mb-3">{{ $totalSubCriteria }}</h1>
-                                        {{-- <div class="mb-0">
-                                            <span class="text-danger">
-                                                <i class="mdi mdi-arrow-bottom-right"></i> -2.25%
-                                            </span>
-                                            <span class="text-muted">Since last week</span>
-                                        </div> --}}
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-                {{-- <div class="col-xl-6 col-xxl-7">
-                    <div class="card flex-fill w-100">
-                        <div class="card-header">
-                            <h5 class="card-title mb-0">Recent Movement</h5>
-                        </div>
-                        <div class="card-body py-3">
-                            <div class="chart chart-sm">
-                                <canvas id="chartjs-dashboard-line"></canvas>
-                            </div>
-                        </div>
-                    </div>
-                </div> --}}
-            </div>
-
-            <div class="row">
-                {{-- <div class="col-12 col-md-6 col-xxl-3 d-flex order-2 order-xxl-3">
-                    <div class="card flex-fill w-100">
-                        <div class="card-header">
-                            <h5 class="card-title mb-0">Browser Usage</h5>
-                        </div>
-                        <div class="card-body d-flex">
-                            <div class="align-self-center w-100">
-                                <div class="py-3">
-                                    <div class="chart chart-xs">
-                                        <canvas id="chartjs-dashboard-pie"></canvas>
-                                    </div>
-                                </div>
-
-                                <table class="table mb-0">
-                                    <tbody>
-                                        <tr>
-                                            <td>Chrome</td>
-                                            <td class="text-end">4306</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Firefox</td>
-                                            <td class="text-end">3801</td>
-                                        </tr>
-                                        <tr>
-                                            <td>IE</td>
-                                            <td class="text-end">1689</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div> --}}
-                {{-- <div class="col-12 col-md-12 col-xxl-6 d-flex order-3 order-xxl-2">
-                    <div class="card flex-fill w-100">
-                        <div class="card-header">
-                            <h5 class="card-title mb-0">Real-Time</h5>
-                        </div>
-                        <div class="card-body px-4">
-                            <div id="world_map" style="height: 350px"></div>
-                        </div>
-                    </div>
-                </div> --}}
             </div>
 
             <div class="row">
@@ -195,7 +101,7 @@
                     </div>
                 </div>
                 <div class="col-12 col-md-6 col-xxl-3 d-flex order-1 order-xxl-1">
-                    <div class="card flex-fill">
+                    <div class="card flex-fill ">
                         <div class="card-header">
                             <h5 class="card-title mb-0">Calendar</h5>
                         </div>
